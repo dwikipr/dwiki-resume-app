79d42eb3e1f409d64a8f3cb4051a2bb13594484a
Dwiki Prastyo <coc.dwiki@gmail.com>
2025-05-12 00:34:00 +0700

    feat(about): add email click handler to contact button
    Added a new function handleEmailClick in the AboutContent component that sets the window location to the user's email address when the "Get In Touch" button is clicked. This enhancement improves user interaction by allowing direct email access through the button.

--- components/AboutContent.tsx
+++ components/AboutContent.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React from 'react';
 
 interface AboutType {
   data: ResumeData;
 }
 
 const AboutContent: React.FC<AboutType> = ({ data }) => {
+  const handleEmailClick = () => {
+    window.location.href = `mailto:${data.profile.email}`;
+  };
+
   return (
     <div className="text-left space-y-2 flex flex-col gap-4">
       <p>{data.summary}</p>
-      <button className="py-2 px-4 bg-black text-white rounded-full text-sm font-semibold cursor-pointer">
+      <button
+        onClick={handleEmailClick}
+        className="py-2 px-4 bg-black text-white rounded-full text-sm font-semibold cursor-pointer"
+      >
         {data.actions.getInTouch}
       </button>
     </div>
